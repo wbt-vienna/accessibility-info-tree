33c674e08bf6cc2ace1e2a1f0ce54c03ba52b476
Benjamin Klaus <klaus@technikum-wien.at>
2020-01-02 15:54:32 +0100

    added caching of tag data

--- src/js/service/data/dataService.js
+++ src/js/service/data/dataService.js
@@ -2,15 +2,27 @@ import {databaseService} from "./databaseService";
 import {Tags} from "../../model/Tags";
 
 let dataService = {};
+let lastTags = null;
+
+//TODO auto-sync
 
 dataService.getTags = function () {
+    if (lastTags) {
+        return Promise.resolve(lastTags);
+    }
     return databaseService.getSingleObject(Tags).then(result => {
-        return Promise.resolve(result ? new Tags(result) : null);
+        lastTags = result ? new Tags(result) : null;
+        return Promise.resolve(lastTags);
     });
 };
 
 dataService.saveTags = function (tagData) {
-    return databaseService.saveObject(Tags, tagData);
+    lastTags = null;
+    let promise = databaseService.saveObject(Tags, tagData);
+    promise.then(() => {
+        lastTags = new Tags(tagData);
+    });
+    return promise;
 };
 
 export {dataService};
