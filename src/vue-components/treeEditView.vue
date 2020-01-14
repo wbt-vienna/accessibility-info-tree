f57ee6f6ebfa3dda6c03e8d9d5a075cec13b58f5
Benjamin Klaus <klaus@technikum-wien.at>
2020-01-14 16:17:50 +0100

    implemented duplicating tags to a specific location, implemented deleting of tags

--- src/vue-components/treeEditView.vue
+++ src/vue-components/treeEditView.vue
@@ -43,10 +43,16 @@
                             thiz.moveMode = true;
                             thiz.moveId = id;
                             thiz.moveParent = parentId;
-                            let invalidMoveTargetIds = tagUtil.getAllChildIds(thiz.moveId, thiz.tags);
-                            thiz.moveTargetIdsChild = thiz.tags.filter(tag => invalidMoveTargetIds.indexOf(tag.id) === -1 && tag.id !== thiz.moveParent).map(tag => tag.id);
+                            let invalidMoveTargetIds = tagUtil.getAllChildIds(thiz.moveId, thiz.tags).concat([thiz.moveId]);
+                            thiz.moveTargetIdsChild = thiz.tags.filter(tag => invalidMoveTargetIds.indexOf(tag.id) === -1 && tag.id !== thiz.moveParent && tag.children.indexOf(thiz.moveId) === -1).map(tag => tag.id);
                             thiz.moveTargetIdsSibling = thiz.tags.filter(tag => invalidMoveTargetIds.indexOf(tag.id) === -1).map(tag => tag.id);
-                            thiz.$refs.notificationBar.setTooltip(`Verschiebe Tag '${tagUtil.getLabel(thiz.moveId, thiz.tags)}'. Ziel wählen oder Abbruch mit [ESC].`);
+                            thiz.$refs.notificationBar.setTooltip(`Verschiebe Tag '${tagUtil.getLabel(thiz.moveId, thiz.tags)}'. Ziel wählen oder Abbruch mit [ESC].`, {
+                                closeable: false,
+                                actionLink: 'Verschieben abbrechen',
+                                actionLinkFn: function () {
+                                    thiz.endMoveMode();
+                                }
+                            });
                         },
                         show: () => !thiz.moveMode
                     },
@@ -54,53 +60,56 @@
                         icon: 'fas fa-arrow-down',
                         title: 'gewählten Tag als Geschwisterknoten dieses Knotens einfügen',
                         fn: function(id, parentId) {
-                            if (id === thiz.moveId) {
-                                thiz.endMoveMode();
-                                return;
-                            }
-                            let targetTag = tagUtil.getTag(id, thiz.tags);
-                            let moveTag = tagUtil.getTag(thiz.moveId, thiz.tags);
-                            let moveParentTag = tagUtil.getTag(thiz.moveParent, thiz.tags);
-                            moveParentTag.children = moveParentTag.children.filter(childID => childID !== moveTag.id);
-                            moveTag.parents = moveTag.parents.filter(parentID => parentID !== moveParentTag.id);
-
-                            let targetParentTag = tagUtil.getTag(parentId, thiz.tags);
-                            targetParentTag.children.push(moveTag.id);
-                            moveTag.parents.push(parentId);
-                            moveTag.parents.sort();
-                            targetParentTag.children.sort();
-                            thiz.endMoveMode();
+                            thiz.insertAsSibling(id, parentId);
                         },
                         show: (id) => thiz.moveMode && thiz.moveTargetIdsSibling.indexOf(id) !== -1
                     },
                     {
                         icon: 'fas fa-arrow-right',
                         title: 'gewählten Tag als Kind dieses Knotens einfügen',
-                        fn: function(id) {
-                            if (id === thiz.moveId) {
-                                thiz.endMoveMode();
-                                return;
-                            }
-                            let targetTag = tagUtil.getTag(id, thiz.tags);
-                            let moveTag = tagUtil.getTag(thiz.moveId, thiz.tags);
-                            let moveParentTag = tagUtil.getTag(thiz.moveParent, thiz.tags);
-                            moveParentTag.children = moveParentTag.children.filter(childID => childID !== moveTag.id);
-                            moveTag.parents = moveTag.parents.filter(parentID => parentID !== moveParentTag.id);
-                            targetTag.children.push(moveTag.id);
-                            moveTag.parents.push(targetTag.id);
-                            targetTag.children = tagUtil.sortTags(targetTag.children, thiz.tags, true);
-                            moveTag.parents = tagUtil.sortTags(moveTag.parents, thiz.tags, true);
-                            thiz.endMoveMode();
+                        fn: function(id, parentId) {
+                            thiz.insertAsChild(id, parentId);
+                        },
+                        show: (id) => thiz.moveMode && thiz.moveTargetIdsChild.indexOf(id) !== -1
+                    },
+                    {
+                        icon: 'fas fa-angle-double-down',
+                        title: 'Kopie des gewählten Tags als Geschwisterknoten dieses Knotens einfügen',
+                        fn: function(id, parentId) {
+                            thiz.insertAsSibling(id, parentId, true);
+                        },
+                        show: (id) => thiz.moveMode && thiz.moveTargetIdsSibling.indexOf(id) !== -1
+                    },
+                    {
+                        icon: 'fas fa-angle-double-right',
+                        title: 'Kopie des gewählten Tags als Kind dieses Knotens einfügen',
+                        fn: function(id, parentId) {
+                            thiz.insertAsChild(id, parentId, true);
                         },
                         show: (id) => thiz.moveMode && thiz.moveTargetIdsChild.indexOf(id) !== -1
                     },
                     {
                         icon: 'fas fa-trash-alt',
                         title: 'Tag Löschen',
-                        fn: function(id) {
-                            thiz.toEditTag(id)
+                        fn: function(id, parentId) {
+                            let deleteTag = tagUtil.getTag(id, thiz.tags);
+                            let backup = JSON.parse(JSON.stringify(thiz.tags));
+                            thiz.$refs.notificationBar.setTooltip(`Tag '${tagUtil.getLabel(deleteTag, thiz.tags)}' gelöscht.`, {
+                                closeable: true,
+                                actionLink: 'Rückgängig machen',
+                                actionLinkFn: function () {
+                                    thiz.tags = backup;
+                                    if (thiz.selectedTag) {
+                                        thiz.selectedTag = tagUtil.getTag(thiz.selectedTag.id, thiz.tags);
+                                    }
+                                }
+                            });
+                            thiz.tags = tagUtil.deleteTag(id, parentId, thiz.tags);
                         },
-                        show: () => !thiz.moveMode
+                        show: (id) => {
+                            let tag = tagUtil.getTag(id, thiz.tags);
+                            return !thiz.moveMode && (tag.children.length === 0 || tag.parents.length > 1)
+                        }
                     }
                 ]
             }
@@ -134,6 +143,46 @@
                 if (keycode === 27) { //ESC
                     thiz.endMoveMode();
                 }
+            },
+            insertAsSibling(id, parentId, copy) {
+                if (id === thiz.moveId) {
+                    thiz.endMoveMode();
+                    return;
+                }
+                let moveTag = tagUtil.getTag(thiz.moveId, thiz.tags);
+                let moveParentTag = tagUtil.getTag(thiz.moveParent, thiz.tags);
+
+                if (!copy) {
+                    moveParentTag.children = moveParentTag.children.filter(childID => childID !== moveTag.id);
+                    moveTag.parents = moveTag.parents.filter(parentID => parentID !== moveParentTag.id);
+                }
+
+                let targetParentTag = tagUtil.getTag(parentId, thiz.tags);
+                targetParentTag.children.push(moveTag.id);
+                moveTag.parents.push(parentId);
+                moveTag.parents = tagUtil.sortTags(moveTag.parents, thiz.tags, true);
+                targetParentTag.children = tagUtil.sortTags(targetParentTag.children, thiz.tags, true);
+                thiz.endMoveMode();
+            },
+            insertAsChild(id, parentId, copy) {
+                if (id === thiz.moveId) {
+                    thiz.endMoveMode();
+                    return;
+                }
+                let targetTag = tagUtil.getTag(id, thiz.tags);
+                let moveTag = tagUtil.getTag(thiz.moveId, thiz.tags);
+                let moveParentTag = tagUtil.getTag(thiz.moveParent, thiz.tags);
+
+                if(!copy) {
+                    moveParentTag.children = moveParentTag.children.filter(childID => childID !== moveTag.id);
+                    moveTag.parents = moveTag.parents.filter(parentID => parentID !== moveParentTag.id);
+                }
+
+                targetTag.children.push(moveTag.id);
+                moveTag.parents.push(targetTag.id);
+                targetTag.children = tagUtil.sortTags(targetTag.children, thiz.tags, true);
+                moveTag.parents = tagUtil.sortTags(moveTag.parents, thiz.tags, true);
+                thiz.endMoveMode();
             }
         },
         mounted() {
