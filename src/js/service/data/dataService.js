528ae2ef496ac74272dbb875210c4ecb9884b821
Benjamin Klaus <klaus@technikum-wien.at>
2020-01-16 17:02:39 +0100

    added CRUD methods for Entry

--- src/js/service/data/dataService.js
+++ src/js/service/data/dataService.js
@@ -2,6 +2,7 @@ import {databaseService} from "./databaseService";
 import {Tags} from "../../model/Tags";
 import $ from "jquery";
 import {constants} from "../../util/constants";
+import {Entry} from "../../model/Entry";
 
 let dataService = {};
 let lastTags = null;
@@ -25,10 +26,35 @@ dataService.saveTags = function (tagData) {
     return promise;
 };
 
+dataService.getEntries = function() {
+    return databaseService.getObject(Entry).then(result => {
+        return Promise.resolve(result ? (result instanceof Array ? result : [result]) : [])
+    });
+};
+
+dataService.getEntry = function (id) {
+    if (!id) {
+        return Promise.resolve(null);
+    }
+    return databaseService.getObject(Entry, id);
+};
+
+dataService.saveEntry = function (entry) {
+    return databaseService.saveObject(Entry, entry);
+};
+
+dataService.remove = function(id) {
+    return databaseService.removeObject(id);
+};
+
 $(document).on(constants.EVENT_DB_PULL_UPDATED, (event, changedDoc) => {
     if (changedDoc.id === constants.TAGS_DOCUMENT_ID) {
         lastTags = changedDoc;
     }
+});
+
+$(document).on(constants.EVENT_DB_UNAUTHORIZED, () => {
+    lastTags = null;
 });
 
 export {dataService};
