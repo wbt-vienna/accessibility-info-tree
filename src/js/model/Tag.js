c89d2f9535230ca9c2e139e045a87cbd19e14bef
Benjamin Klaus <klaus@technikum-wien.at>
2020-02-07 10:39:33 +0100

    added new fields to Tag model

--- src/js/model/Tag.js
+++ src/js/model/Tag.js
@@ -6,6 +6,10 @@ let Tag = ObjectModel({
     modelName: [String],
     label: [Object],
     color: [String],
+    searchRoot: [Boolean], //if true children are shown in tag search
+    notAssignable: [Boolean], //if true this tag is not directly assignable to an entry, only children
+    mandatory: [Boolean], //mandatory that a child of this tag is tagged
+    optional: [Boolean], //optional that a child of this tag is tagged (set to true in order to add this tag to "optional" section in create entry view)
     parents: ArrayModel(String),
     children: ArrayModel(String),
 }).defaultTo({
